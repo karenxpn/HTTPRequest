83124f1bad02ee3326bea2e6b27de051c782043d
KALIMI <karenmirakyan@gmail.com>
2021-03-23 23:11:39 +0400

    Initial Commit

---
+++ HTTPRequest/HTTPRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPRequestApp.swift
+//  HTTPRequest
+//
+//  Created by Karen Mirakyan on 23.03.21.
+//
+
+import SwiftUI
+
+@main
+struct HTTPRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
